169e984cfcadf1f934b259354145ee1b49611d9b
Danilo Margarido <bit2shift@gmail.com>
2017-05-11 13:52:25 +0100

    SMF's user class

---
+++ Widget/autoauth/hooks/SMFUser.php
@@ -0,0 +1,33 @@
+<?php
+namespace hooks;
+
+class SMFUser extends AUser
+{
+	/**
+	 * Create a new SMFUser
+	 * @param string $boarddir path to the forum's folder
+	 */
+	function __construct($boarddir)
+	{
+		if(isset($_GET['ssi_function']))
+		{
+			unset($_GET['ssi_function']);
+			return header('Location: ?' . http_build_query($_GET));
+		}
+
+		require("$boarddir/SSI.php");
+	}
+
+	function name()
+	{
+		global $user_info;
+		return $user_info['username'];
+	}
+
+	function role()
+	{
+		global $user_info;
+		return ($user_info['is_admin'] ? 'admin' : ($user_info['is_guest'] ? 'guest' : 'user'));
+	}
+}
+?>
