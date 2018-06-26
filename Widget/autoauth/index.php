d084741d273f0f808720ef88e2dca7a21967db74
Danilo Margarido <bit2shift@gmail.com>
2018-06-26 22:27:22 +0100

    Now that's a template
    Still a work in progress. I might end up using Plates

--- Widget/autoauth/index.php
+++ Widget/autoauth/index.php
@@ -7,23 +7,23 @@ if(!$config->hook instanceof autoauth\hook\IHook)
 	die(sprintf('"%s" is not a valid hook, it does not implement "%s".', get_class($config->hook), autoauth\hook\IHook::class));
 }
 
-$layout = new autoauth\layout\Page("pages/index.{$config->hook->userRole()}.xml");
-
 if($_SERVER['REQUEST_METHOD'] === 'POST')
 {
 	$name = $config->hook->userName();
 
 	if($config->hook->userRole() === 'admin')
-	{
 		$name = $_POST['name'] ?: $name;
-		$layout->{'//html:input[@maxlength]'}->item(0)->setAttribute('value', $name);
-	}
 
 	if(strlen($name))
 	{
 		//TODO
-		$layout->{'//html:input[@readonly]'}->item(0)->setAttribute('value', "UUID for '$name'");
+		$uuid = "UUID for '$name'";
 	}
 }
+else
+{
+	$name = '';
+	$uuid = '';
+}
 
-$layout->display();
+require('pages/index.xml');
